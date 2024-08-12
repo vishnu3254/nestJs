0e6610958a32766527ca0de24aba52379b59d55b
vishnu3254 <vishnuvardhanudagundla@gmail.com>
2024-08-12 15:45:02 +0530

    changes iupated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [UsersModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
-import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [],
   controllers: [EmployeesController],
   providers: [EmployeesService],
 })
